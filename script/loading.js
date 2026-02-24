720ae68b2400648f3975fb1d1f252d7bf7c0d6c1
BreeAlexander <Alexanderbree095@gmail.com>
2026-02-24 18:24:08 +0000

    Tue, Feb 24, 2026, 10:24 AM -08:00

--- script/loading.js
+++ script/loading.js
@@ -9,7 +9,11 @@ function blurring() {
     load = load + 1
     loadText.innertext = `${load}%`;
     loadText.style.opacity = 1-load/100
-    bg.style.filter= `blur($(50-load/2)p
+    bg.style.filter= `blur($(50-load/2)px`
+
+    if (load > 99) {
+        clear Interval(int)
+    }
 }
 
 
