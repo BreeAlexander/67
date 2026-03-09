c1e91a2b82fa149e141bb806df986a4846acdb57
BreeAlexander <Alexanderbree095@gmail.com>
2026-03-09 16:46:17 +0000

    Mon, Mar 9, 2026, 9:46 AM -07:00

--- script/event.js
+++ script/event.js
@@ -1,5 +1,9 @@
 clickbox = document.querySelector('.click')
 spinbox = document.querySelector('.spin')
+movebox = document.querySelector('.move')
+hoverbox = document.querySelector('.hover')
+dblclickbox = document.querySelector('.dblclick')
+
 
 colors = ['blue', 'green', 'purple', 'orange', 'red', 'pink', 'brown', 'black']
 index = 0
