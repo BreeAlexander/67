df61dee08c144ce66b501bba5ef828e3d8c2d6cb
BreeAlexander <Alexanderbree095@gmail.com>
2026-03-11 16:36:27 +0000

    Wed, Mar 11, 2026, 9:36 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -1,10 +1,21 @@
 buttons = document.querySelector('.buttons')
 applause = document.querySelector('.applause')
+boo = document.querySelector('.boo')
+
 
 btn = document.createElement('button')
 btn.innerText = "Applause"
 btn.classList.add('btn')
 btn.addEventListener('click', () => {
     applause.play()
+})
+buttons.appendChild(btn)
+
+
+btn = document.createElement('button')
+btn.innerText = "Boo"
+btn.classList.add('btn')
+btn.addEventListener('click', () => {
+    boo.play()
 })
 buttons.appendChild(btn)
