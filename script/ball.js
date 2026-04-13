32d4d8928e3f28d9b75056328f44bc2c32fe5fc6
BreeAlexander <Alexanderbree095@gmail.com>
2026-04-13 16:32:18 +0000

    Mon, Apr 13, 2026, 9:32 AM -07:00

--- script/ball.js
+++ script/ball.js
@@ -16,7 +16,7 @@ function moveBall(){
     ball.style.left = `${ballXPosition}px`
     ball.style.top = `${ballYPosition}px`
     if (ballYPosition < 0 || ballYPosition > windowHeight - 2 * ballRadius) {
-        ballXDirection = ballXDirection * -1
+        ballYDirection = ballYDirection * -1
     }
     if (ballXPosition < 0 || ballXPosition > windowWidth - 2 * ballRadius) {
         ballXDirection = ballXDirection * -1
