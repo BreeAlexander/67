c521d074f382c4b4ddf34e4afe57a07868e7af54
BreeAlexander <Alexanderbree095@gmail.com>
2026-04-15 17:15:56 +0000

    Wed, Apr 15, 2026, 10:15 AM -07:00

--- script/ball.js
+++ script/ball.js
@@ -116,3 +116,9 @@ animate()
 
 // right side ____
 
+const Rpadel = document.createElement('div')
+let RpadelWidth = 20
+let RpadelHeight = 100
+let RpadelSpeed = 20
+let RpadelYPosition = windowHeight / 2 - LpadelHeight / 2
+let RpadelXPosition = 70
